fdb3b49760e5407443540076fa75df0aae73ad74
swellbuilt <aaronbowden@iMac-1TB.local>
2016-07-21 13:40:37 -0400

    On Ready

--- indeed-consulate.js
+++ indeed-consulate.js
@@ -1,16 +1,19 @@
- var indeedScript = "<script type='text/javascript'>" +
-  "var indeed_conversion_id = '4552366420437082'; " +
-  "var indeed_conversion_label = '';" +
-  "</script>" +
-  "<script type='text/javascript' src='//conv.indeed.com/pagead/conversion.js'></script>" +
-  "<noscript>" +
-  "<img height=1 width=1 border=0 src='//conv.indeed.com/pagead/conv/4552366420437082/?script=0'>" +
-  "</noscript>";
+var indeedScript = "<script type='text/javascript'>" +
+"var indeed_conversion_id = '4552366420437082'; " +
+"var indeed_conversion_label = '';" +
+"</script>" +
+"<script type='text/javascript' src='//conv.indeed.com/pagead/conversion.js'></script>" +
+"<noscript>" +
+"<img height=1 width=1 border=0 src='//conv.indeed.com/pagead/conv/4552366420437082/?script=0'>" +
+"</noscript>";
   
-  var txtApplied = "Your application has been received.";  
-  if ($('[id^=apply_jobapp]:contains(" + txtApplied + ")').length) {
+var txtApplied = "Your application has been received."; 
+jQuery(function($) {
+	if ($('[id^=apply_jobapp]:contains(" + txtApplied + ")').length) {
     alert('Application received');
     var div = document.createElement('div');
     div.innerHTML = indeedScript;
     document.getElementsByTagName('body')[0].appendChild(div);
-  }
+	}
+});
+
