990bd8ea470ea32365b66499471fca9a295e5063
swellbuilt <aaronbowden@iMac-1TB.local>
2016-07-21 13:33:46 -0400

    Indeed Tracking Code

---
+++ indeed-consulate.js
@@ -0,0 +1,16 @@
+ var indeedScript = "<script type='text/javascript'>" +
+  "var indeed_conversion_id = '4552366420437082'; " +
+  "var indeed_conversion_label = '';" +
+  "</script>" +
+  "<script type='text/javascript' src='//conv.indeed.com/pagead/conversion.js'></script>" +
+  "<noscript>" +
+  "<img height=1 width=1 border=0 src='//conv.indeed.com/pagead/conv/4552366420437082/?script=0'>" +
+  "</noscript>";
+  
+  var txtApplied = "Your application has been received.";  
+  if ($('[id^=apply_jobapp]:contains(" + txtApplied + ")').length) {
+    console.log('Application received');
+    var div = document.createElement('div');
+    div.innerHTML = indeedScript;
+    document.getElementsByTagName('body')[0].appendChild(div);
+  }
