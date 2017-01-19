ee604b69d004eb7ac0a69dd1b10c1312ff59ad74
swellbuilt <info@swellbuilt.com>
2017-01-19 16:00:37 -0500

    Update huemanriskadjustment.js

--- huemanriskadjustment.js
+++ huemanriskadjustment.js
@@ -1,6 +1,7 @@
 console.log('external');
 
-var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + window.location.pathname.indexof;
+//var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + window.location.pathname.indexof;
+var newurl = window.location.href;
 window.history.pushState({path:newurl}, '', newurl + "&searchdropcity=AL$$Birmingham");
 
 //var myURL = document.location;
