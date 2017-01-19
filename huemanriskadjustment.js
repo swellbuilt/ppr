779b8ab7f99868609d2c4c4943f9dd05ec5ce99a
swellbuilt <info@swellbuilt.com>
2017-01-19 15:08:54 -0500

    Update huemanriskadjustment.js

--- huemanriskadjustment.js
+++ huemanriskadjustment.js
@@ -1,5 +1,5 @@
 console.log('external');
-var myURL = document.location;
-if (!window.location.search.indexOf('searchdropcity=AL$$Birmingham') > -1) {
-  document.location = myURL + "&searchdropcity=AL$$Birmingham";
-}
+//var myURL = document.location;
+//if (!window.location.search.indexOf('searchdropcity=AL$$Birmingham') > -1) {
+  //document.location = myURL + "&searchdropcity=AL$$Birmingham";
+//}
