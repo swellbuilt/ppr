90bcd7244e1808cce9a6361fe9d1ec7311608225
swellbuilt <info@swellbuilt.com>
2017-01-21 10:49:39 -0500

    Update huemanriskadjustment.js

--- huemanriskadjustment.js
+++ huemanriskadjustment.js
@@ -2,9 +2,10 @@ var newurl;
 function refURL(newurl) {
   console.log('external');
   if (newurl === undefined) {
-    var newurl = window.location.href;
+    
+  } else {
+    document.history.pushState({path:newurl}, '', newurl + "&searchdropcity=AL$$Birmingham");
   }
-  window.history.pushState({path:newurl}, '', newurl + "&searchdropcity=AL$$Birmingham");
   //var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + window.location.pathname.indexof;
   //if (!window.location.search.indexOf('searchdropcity=AL$$Birmingham') > -1) {
   //}
